ab49e2c5a3e75e3df86f316e0624f869ae35b0c5
fu-01 <1787276972@qq.com>
2020-06-09 15:06:56 +0800

    first commit

---
+++ src/components/all.vue
@@ -0,0 +1,15 @@
+<template>
+    <info :data="$store.state.todos"></info>
+</template>
+<script>
+import info from './info.vue'
+export default {
+    components:{
+        info
+    }
+}
+</script>
+
+<style>
+    
+</style>
